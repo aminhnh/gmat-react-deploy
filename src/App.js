7eb9f5c884c70c5a4d24a5e3a155021832c02626
Aminah Huda <aminahnurulhuda@mail.ugm.ac.id>
2022-11-14 23:07:27 +0700

    Added socket io

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Map from "./Map.js";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(function () {
+    const socket = io("https://gmat-backend.herokuapp.com/");
+
+    socket.on("DATA", function (payload) {
+      console.log(payload);
+    });
+  });
   return (
     <div className="App">
-      <h2>GMAT Software Programmer Instership FInal Project</h2>
+      <h2>GMAT Software Programmer Instership Final Project</h2>
       <h3>
         Aminah Nurul Huda <br></br> 22/494455/SV/20830
       </h3>
