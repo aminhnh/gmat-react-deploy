d125bd8e16a56921d87b3e68527e4186d1e58cda
Aminah Huda <aminahnurulhuda@mail.ugm.ac.id>
2022-11-14 11:16:43 +0700

    Added a map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function App() {
   return (
     <div className="App">
       <h1>this is working, yeah</h1>
+      <MapContainer
+        style={{ height: 400 }}
+        center={[-7.775193, 110.374234]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-7.775193, 110.374234]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
