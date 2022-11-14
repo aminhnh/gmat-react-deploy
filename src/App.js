cd5ec88949f5598cba8a99715b298a3e25568420
Aminah Huda <aminahnurulhuda@mail.ugm.ac.id>
2022-11-14 22:14:57 +0700

    Changed map to a component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import "./App.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Map from "./Map.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>this is working, yeah</h1>
-      <MapContainer
-        style={{ height: 400 }}
-        center={[-7.775193, 110.374234]}
-        zoom={13}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[-7.775193, 110.374234]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      <h2>GMAT Software Programmer Instership FInal Project</h2>
+      <h3>
+        Aminah Nurul Huda <br></br> 22/494455/SV/20830
+      </h3>
+      <Map />
     </div>
   );
 }
