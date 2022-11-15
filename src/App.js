07d0df51ad2703b9502223036cb946b437904961
Aminah Huda <aminahnurulhuda@mail.ugm.ac.id>
2022-11-15 17:09:07 +0700

    Chart progress

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import "./App.css";
-import Map from "./Map.js";
+import Map from "./components/Map.js";
 import { io } from "socket.io-client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Chart1 from "./components/Chart1.js";
 
 function App() {
-  useEffect(function () {
-    const socket = io("https://gmat-backend.herokuapp.com/");
+  // useEffect(function () {
+  //   const socket = io("https://gmat-backend.herokuapp.com/");
+  //   socket.on("DATA", function (payload) {
+  //     console.log(payload);
+  //   });
+  // });
+  const [socket, setSocket] = useState(null);
+  const [data, setData] = useState();
 
+  const [voltages, setVoltages] = useState([]);
+  // const [preassures, setPreasures] = useState([]);
+  const [clocks, setClocks] = useState([]);
+
+  useEffect(
+    function () {
+      const socket = io("https://gmat-backend.herokuapp.com/");
+      console.log("connected");
+      setSocket(socket.io);
+      if (socket) return;
+    },
+    [socket]
+  );
+
+  useEffect(function () {
+    if (!socket) return;
     socket.on("DATA", function (payload) {
-      console.log(payload);
+      const dataArr = payload.slice(0, -1).split(",");
+
+      const dataSensor = {
+        TEAM_ID: dataArr[0],
+        CLOCK: dataArr[1],
+        YAW: Number(dataArr[2]),
+        PITCH: Number(dataArr[3]),
+        ROLL: Number(dataArr[4]),
+        LATITUDE: Number(dataArr[5]),
+        LONGITUDE: Number(dataArr[6]),
+        VOLTAGE: Number(dataArr[7]),
+        PRESSURE: Number(dataArr[8]),
+        ALTITUDE: Number(dataArr[9]),
+      };
+
+      setVoltages((voltages) => [...voltages, dataSensor.VOLTAGE]);
+      setClocks((clocks) => [...clocks, dataSensor.CLOCK]);
+
+      setData(dataSensor);
     });
   });
 
@@ -18,7 +59,9 @@ function App() {
       <h3>
         Aminah Nurul Huda <br></br> 22/494455/SV/20830
       </h3>
+      <h2>Latitude: {data?.LATITUDE}</h2>
       <Map />
+      <Chart1 data={voltages} label={clocks} title="Voltage" />
     </div>
   );
 }
---
+++ src/components/Chart1.js
@@ -0,0 +1,111 @@
+import * as echarts from "echarts";
+import { useEffect, useRef, useState } from "react";
+
+const option = {
+  title: {
+    text: "NAMA CHARTNYA",
+  },
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      animation: false,
+    },
+  },
+  xAxis: {
+    type: "time",
+    splitLine: {
+      show: false,
+    },
+  },
+  yAxis: {
+    type: "value",
+    boundaryGap: [0, "100%"],
+    splitLine: {
+      show: false,
+    },
+  },
+  series: [
+    {
+      name: "Fake Data",
+      type: "line",
+      showSymbol: false,
+      data: [],
+    },
+  ],
+};
+
+export default function Chart1({ data, label, title = "Chart" }) {
+  const [chart, setChart] = useState();
+  const chartRef = useRef();
+
+  useEffect(
+    function () {
+      if (!chartRef.current) return;
+
+      const chart = echarts.init(chartRef.current);
+
+      chart.setOption(option);
+      chart.setOption({
+        title: {
+          text: title,
+        },
+      });
+      setChart(chart);
+
+      window.addEventListener("resize", function () {
+        chart.resize();
+      });
+    },
+    [chartRef, title]
+  );
+
+  useEffect(() => {
+    if (!chart) return;
+    console.log(data, label);
+
+    const dataObject = [];
+
+    for (let i = 0; i < data.length; i++) {
+      if (dataObject.length > 15) {
+        dataObject.shift();
+      }
+
+      const formattedDate = formatDate(label[i]);
+      dataObject.push({
+        name: formattedDate,
+        value: [formattedDate, data[i]],
+      });
+    }
+
+    chart.setOption({
+      series: [
+        {
+          data: dataObject,
+        },
+      ],
+    });
+  }, [chart, data, label]);
+
+  function formatDate(dateHHMMSS) {
+    const dateArr = dateHHMMSS.split(":");
+    const now = new Date();
+    now.setHours(dateArr[0]);
+    now.setMinutes(dateArr[1]);
+    now.setSeconds(dateArr[2]);
+
+    const formattedDate = `${now.getFullYear()}/${now.getMonth()}/${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+    return formattedDate;
+  }
+
+  return (
+    <div
+      ref={chartRef}
+      id="line-chart"
+      style={{
+        height: "200px",
+        width: "100%",
+      }}
+    ></div>
+  );
+}
